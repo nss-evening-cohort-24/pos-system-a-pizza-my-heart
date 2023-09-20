f4fc87c8da37f2f31a0daefc802484009fb85283
Lucas-Martin2023 <lucasmartin11990@gmail.com>
2023-09-20 18:45:15 -0500

    committing changes

--- pages/renderHomePage.js
+++ pages/renderHomePage.js
@@ -4,6 +4,9 @@ import renderToDOM from '../utils/renderToDom';
 const renderHomePage = () => {
   navBar();
   const domString = `
+  <div id="welcomeMessage">
+    <h1>Welcome {Insert User's Name Here}</h1>
+  </div>
     <button type="button" class="btn btn-primary btn-lg" id="viewOrderBtn">View Orders</button>
     <button type="button" class="btn btn-success btn-lg" id="createOrderBtn">Create an Order</button>
     <button type="button" class="btn btn-info btn-lg" id="viewRevenueBtn">View Revenue</button>`;
