777fd10c91032f7bbd36e208f1525fae1d2354b2
Lucas-Martin2023 <lucasmartin11990@gmail.com>
2023-09-20 21:29:54 -0500

    committing to get austins changes

--- utils/startApp.js
+++ utils/startApp.js
@@ -3,10 +3,12 @@ import domBuilder from '../components/shared/domBuilder';
 import renderHomePage from '../pages/renderHomePage';
 import navigationEvents from '../events/navigationEvents';
 import addEvents from '../events/domEvents';
+import renderEditItemPage from '../pages/renderEditItemPage';
 
 const startApp = () => {
   domBuilder();
   renderHomePage();
+  renderEditItemPage();
   navigationEvents();
   addEvents();
   logoutButton();
