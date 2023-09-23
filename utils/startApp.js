06b3dbc2c0ddd2d41790431f29be866c30096ba9
Lucas-Martin2023 <lucasmartin11990@gmail.com>
2023-09-22 23:11:12 -0500

    pushing stuff

--- utils/startApp.js
+++ utils/startApp.js
@@ -3,17 +3,11 @@ import domBuilder from '../components/shared/domBuilder';
 import renderHomePage from '../pages/renderHomePage';
 import navigationEvents from '../events/navigationEvents';
 import addEvents from '../events/domEvents';
-// import renderCloseOrderPage from '../pages/renderCloseOrderPage';
 
 const startApp = (user) => {
   domBuilder();
   renderHomePage(user);
-  // getItems('-NejoBZ57ughztNDiuOe').then((array) => {
-  //   console.warn(array);
-  //   renderOrderDetailsPage('-NejoBZ57ughztNDiuOe', array);
-  // });
   navigationEvents(user);
-  // renderCloseOrderPage();
   addEvents(user);
   logoutButton();
 };
